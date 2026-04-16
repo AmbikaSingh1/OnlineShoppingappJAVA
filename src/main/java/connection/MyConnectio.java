8f79212a8b15630cc2e98528031eb2005c3f1c9e
AmbikaSingh1 <ambikathakur013@gmail.com>
2026-04-16 21:46:26 +0530

    Shopping_app

---
+++ src/main/java/connection/MyConnectio.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connection;
+
+/**
+ *
+ * @author vidhi
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyConnectio {
+       public static Connection getConnection(){
+Connection con=null;
+try{
+Class.forName("com.mysql.cj.jdbc.Driver");
+con=DriverManager.getConnection("jdbc:mysql://localhost:3306/shopping_db");
+}
+catch(Exception ex){
+    System.out.println("Connection Error : "+ ex.getMessage());
+}
+return con;
+
+}    
+}
